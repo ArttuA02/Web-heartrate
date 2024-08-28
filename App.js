5549cfdeb0a194fc36149571c8ee939cf2088118
arttu aalto <t1aaar01@students.oamk.fi>
2024-08-28 17:48:28 +0300

    viikkoteht 1

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [hr, setHr] = useState('')
+
+  const calculateHr = (text) => {
+    setAge(text)
+    const ageNumber = parseFloat(text.replace(',', '.'));
+    
+    const minHr = (220 - ageNumber) * 0.65
+    const maxHr = (220 - ageNumber) * 0.85
+    setHr(`${minHr} - ${maxHr}`)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+      <TextInput
+        style={styles.field}
+        value={age}
+        onChangeText={calculateHr}
+        keyboardType='decimal-pad'
+      />
+
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{hr}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
